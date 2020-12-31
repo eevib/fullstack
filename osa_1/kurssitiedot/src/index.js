839e6b6101bee2d7cb8845fb51a38757ab04d4d4
Eevi Bengs <eevi.bengs@helsinki.fi>
2020-12-31 14:15:17 +0200

    1.5 kurssitiedot step5

--- osa_1/kurssitiedot/src/index.js
+++ osa_1/kurssitiedot/src/index.js
@@ -2,27 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course =  {
+    name: 'Half Stack application development',
+    parts: [
     {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
   ]
+}
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
    </div>
   )
 }
