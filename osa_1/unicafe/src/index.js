72a2c70572e86b62281b74c9f5eef253dd0f552f
Eevi Bengs <eevi.bengs@helsinki.fi>
2020-12-31 17:33:30 +0200

    1.10 unicafe step3-step5

--- osa_1/unicafe/src/index.js
+++ osa_1/unicafe/src/index.js
@@ -1,8 +1,40 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad
+  if(total === 0) {
+    return (
+      <div>No feedback given</div>   
+    )   
+  }
+  return (
+    <div>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />  
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={(props.good-props.bad)/total} />
+      <StatisticLine text="positive" value={props.good/total*100} />    
+    </div>
+  )
+}
+const StatisticLine = (props) => {
+  if(props.text==="positive") {
+    return (
+      <div>
+        {props.text} {props.value} % <br></br>
+      </div>
+    )
+  }
+  return (
+    <div>
+      {props.text} {props.value} <br></br>
+    </div>
+  )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -14,12 +46,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
       <Button handleClick={() => setBad(bad +1)} text="bad"/>
       <h1>statistics</h1>
-      good {good} <br></br>
-      neutral {neutral} <br></br>
-      bad {bad} <br></br>
-      <Total good={good} neutral={neutral} bad={bad}/>
-      <Average good={good} neutral={neutral} bad={bad} />
-      <Positive good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
@@ -28,36 +55,7 @@ const Button = (props) => {
   <button onClick={props.handleClick}>{props.text}</button>
   )
 }
-const Average=(props) => {
-  const total = props.good + props.neutral + props.bad
-  if(total == 0) {
-    return <p>average</p>
-  }
-  return (
-    <div>
-      average {(props.good - props.bad)/total}
-    </div>
-    )
-}
 
-const Total=(props) => {
-  return (
-  <div>
-    all {props.good + props.neutral + props.bad}
-  </div>
-  )
-}
-const Positive=(props) => {
-  const total = props.good + props.neutral + props.bad
-  if(total == 0) {
-    return <p>positive</p>
-  }
-  return (
-  <div>
-    positive {props.good/total*100} %
-  </div>
-  )
-}
 ReactDOM.render(<App />, 
   document.getElementById('root')
 )
