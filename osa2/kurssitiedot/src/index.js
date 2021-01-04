9b56325241f8500cb06d484bf48dcdcc6a529cf0
Eevi Bengs <eevi.bengs@helsinki.fi>
2021-01-04 08:58:10 +0200

    2.5 kurssitiedot

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -4,7 +4,8 @@ import Course from './components/Course'
 
  
 const App = () => {
-  const course = {
+  const courses = [
+    {
     name: 'Half Stack application development',
     id: 1,
     parts: [
@@ -22,12 +23,37 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
-  }
+  }, 
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+      ]
+    }
+  ]
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
+        <Course key={course.id} course={course} />
+        )}
     </div>
   )
 }
